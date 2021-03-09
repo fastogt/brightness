d66df8db60f8a8e960fc7f6968e7cf50def9c3b9
Вадим Турко <vadim.turko@gmail.com>
2021-03-09 11:04:40 +0300

    init

---
+++ ios/Classes/BrightnessPlugin.m
@@ -0,0 +1,20 @@
+#import "BrightnessPlugin.h"
+
+@implementation BrightnessPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"brightness"
+            binaryMessenger:[registrar messenger]];
+  BrightnessPlugin* instance = [[BrightnessPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
