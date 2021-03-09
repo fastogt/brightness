ece835372a3306f9ec40800b5e871b2e706ebb16
Вадим Турко <vadim.turko@gmail.com>
2021-03-09 11:31:13 +0300

    plugin null-safety

--- lib/brightness.dart
+++ lib/brightness.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class Screen {
   static const MethodChannel _channel = const MethodChannel('github.com/fastogt/brightness');
 
-  static Future<double> get brightness async {
-    return (await _channel.invokeMethod('brightness')) as double;
+  static Future<double?> get brightness async {
+    return (await _channel.invokeMethod('brightness')) as double?;
   }
 
   static Future<void> setBrightness(double brightness) {
